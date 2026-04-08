28589bed051d09b011888db80cdd8db3a9ac4923
asiri-gayashan <gayashanx@gmail.com>
2026-04-08 23:57:11 +0530

    fix: correct formatting of the endpoint to fetch posts

--- server.js
+++ server.js
@@ -4,7 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Define endpoint to fetch posts
-
+app.get("/posts", async (req, res) => {
+  try {
+    const responce = await fetch(
+      "https://jsonplaceholder.typicode.com/posts?_limit=1"
+    );
+    const data = await responce.json();
+    res.json(data);
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    res.status(500).json({ error: "Error fetching posts" });
+  }
+});
 
 // Define endpoint to fetch post by id as query parameter
 app.get("/posts/:id", async (req, res) => {
