9affbb5ee1a4668456cc3e71be9c29704aa95d8c
asiri-gayashan <gayashanx@gmail.com>
2026-04-23 22:51:33 +0530

    chore: remove endpoint to fetch post by id from server.js

--- server.js
+++ server.js
@@ -17,21 +17,7 @@ app.get("/posts", async (req, res) => {
   }
 });
 
-// Define endpoint to fetch post by id as query parameter
-app.get("/posts/:id", async (req, res) => {
-  const { id } = req.params;
-  try {
-    const responce = await fetch(
-      `https://jsonplaceholder.typicode.com/posts/${id}`
-    );
 
-    const data = await responce.json();
-    res.json(data);
-  } catch (error) {
-    console.error("Error fetching post by id:", error);
-    res.status(500).json({ error: "Error fetching post by id" });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
